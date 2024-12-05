8650e2ccc471c35eece266be7a8ca1bf7521b6db
Jimmie Fulton <jimmie.fulton@gmail.com>
2024-12-05 15:11:26 -0800

    Add conditional support for both MacOS and Linux on Homebrew

--- Formula/archetect.rb
+++ Formula/archetect.rb
@@ -4,12 +4,28 @@
 class Archetect < Formula
   desc "A powerful code-centric, language agnostic content generator"
   homepage "https://github.com/archetect/archetect"
-  url "https://github.com/archetect/archetect/releases/download/v2.0.0/archetect-v2.0.0-macos_aarch64.tar.gz"
-  sha256 "4e854d18dd1fbcf126c813a3cbc93c088a6b0b5faedfef7e02eb0c6b7309c9d9"
   version "2.0.0"
 
-  def install
-    bin.install "archetect"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/archetect/archetect/releases/download/v#{version}/archetect-v#{version}-macos_aarch64.tar.gz"
+      sha256 "4e854d18dd1fbcf126c813a3cbc93c088a6b0b5faedfef7e02eb0c6b7309c9d9"
+
+      def install
+        bin.install "archetect"
+      end
   end
+
+  on_linux do
+    if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
+      url "https://github.com/archetect/archetect/releases/download/v#{version}/archetect-v#{version}-linux_x64.tar.gz"
+      sha256 "d5f3495dde91e78ecb2eca1920594fd9460c58a413f68b13e704ec0d4a3ac0b6"
+
+      def install
+        bin.install "archetect"
+      end
+  end
+
+
 end
 
